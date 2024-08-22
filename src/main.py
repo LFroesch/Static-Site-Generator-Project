ca845d74c1c27b0c79350fc826f666ba90da853a
LFroesch <lucas.froeschner@gmail.com>
2024-08-22 14:26:34 -0700

    init git for project

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+print("Init Main")
+
+def main():
+    test = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(test)
+
+if __name__ == "__main__":
+    main()
